8f54e2dfa08bb5f9d4c3c2e39bcbd0946791232b
Fco. Javier Mesa Olmo <francisco.mesa@ebury.com>
2022-01-17 13:30:13 +0100

    Create book event data source to pass data to a component

---
+++ src/components/events/events.vue
@@ -0,0 +1,40 @@
+<script>
+import useEventCapacity from '../../uses/use-event-capacity';
+import { delay } from "../../services/stubs";
+
+export default {
+  props: {
+    msg: String,
+  },
+
+  async setup(props, context) {
+    await delay(2000);
+
+    return {
+      ...useEventCapacity(),
+    };
+  }
+}
+</script>
+
+<template>
+  <div class="events">
+    <h3>{{ msg }}</h3>
+
+    <p>Capacity: {{ capacity }}</p>
+
+    <button class="events__btn" type="button" @click="increaseCapacity()">Increase</button>
+    <button class="events__btn" type="button" @click="decreaseCapacity()">Decrease</button>
+    <button type="button" :disabled="isBookEventDisabled" @click="book()">Book</button>
+
+    <p>Result: {{ bookEventDataSource.result.value }}</p>
+    <p>Loading: {{ bookEventDataSource.loading.value }}</p>
+    <p>Error: {{ bookEventDataSource.error.value }}</p>
+  </div>
+</template>
+
+<style scoped>
+.events__btn {
+  margin-right: 8px;
+}
+</style>
